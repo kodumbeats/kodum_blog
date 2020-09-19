a34abe112dd65ad815338ccd4443abfa529578e2
kodumbeats <brandon.leckemby@gmail.com>
2020-09-18 22:53:22 -0400

    remove pug and styling

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,15 +1,24 @@
-<template lang="pug">
-div.wholething
-  div
-    h1 > kodum_blog
-      span.cursor |
-  <Nuxt />
-  footer.
-    #[a(href="https://leckemby.me") about]
-    #[a(href="mailto: brandon.leckemby@gmail.com") email]
-    #[a(href="https://www.linkedin.com/in/brandonleckemby") linkedin]
-    #[a(href="https://github.com/kodumbeats") github]
-    <br> &#169; 2020 kodumdotnet
+<template>
+  <div class="wholething">
+    <mq-layout mq="md+">
+      <div class="header">
+        <img class="header" src="/dotmeslashblog_4x.png" />
+      </div>
+    </mq-layout>
+    <mq-layout mq="sm">
+      <h1 class="smallheader">kodum_blog _</h1>
+      <hr class="hr1" />
+    </mq-layout>
+    <Nuxt />
+    <footer>
+      <a href="https://leckemby.me">home</a>
+      <a href="mailto: brandon.leckemby@gmail.com">email</a>
+      <a href="https://github.com/kodumbeats">github</a>
+      <a href="https://www.linkedin.com/in/brandonleckemby">linkedin</a>
+      <br />
+      &#169; 2020 kodumdotnet
+    </footer>
+  </div>
 </template>
 
 <style scoped>
@@ -19,14 +28,23 @@ div.wholething
 h1 {
   color: #ffffff;
   font-family: 'Changa', sans-serif;
+  font-size: 2.8rem;
+  margin-left: 10px;
 }
-.cursor {
-  animation: blinker 1.2s step-end infinite;
+a {
+  font-family: 'Changa', sans-serif;
 }
-@keyframes blinker {
-  50% {
-    opacity: 0;
-  }
+.header {
+  display: block;
+  margin: 0 auto 1rem 4rem;
+  padding: 1rem 0;
+  height: 200px;
+}
+.smallheader {
+  padding-left: 1rem;
+}
+.hr1 {
+  border: 2px solid #222579;
 }
 footer {
   text-align: center;
